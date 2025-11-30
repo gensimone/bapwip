f0558d6950b00c4ed21546bddf8592b06e825810
Simone Gentili <gensimone.git@gmail.com>
2025-11-30 15:40:14 +0100

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # the command used to generate the python-packages.nix file
+  # nix run github:nix-community/pip2nix -- generate innertube
+  # packageOverrides = pkgs.callPackage ./python-packages.nix {};
+  # python = pkgs.python3.override { inherit packageOverrides; };
+  python = pkgs.python3;
+in
+  pkgs.mkShell {
+    packages = with pkgs; [
+      (python.withPackages(p: with p; [
+        customtkinter
+        mpv
+        pillow
+        tkinter
+      ]))
+    ];
+  }
